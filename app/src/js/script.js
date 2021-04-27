37b85cc9617d8668a8fad1d663b67c41bf60ab4c
shoffkarim <k4r1mch1k@gmail.com>
2021-04-27 16:03:06 +0500

    скролл видео

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,2 +1,7 @@
+/* eslint-disable no-unused-vars */
 // STYLES=============================================================================
 import '../sass/style.sass';
+
+import Video from "./modules/video";
+
+const video = new Video();
