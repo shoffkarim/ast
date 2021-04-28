46c1585991f2ee16a3341d3075d307af1cb7e83a
shoffkarim <k4r1mch1k@gmail.com>
2021-04-28 13:36:08 +0500

    анимация здания и машин на scrollMagic

--- app/src/js/modules/building.js
+++ app/src/js/modules/building.js
@@ -25,22 +25,36 @@ class Building {
 }
 
 function classManager(wrap, clientHeight) {
+  // console.log(window.scrollY);
   const control = document.querySelector(".js-building-control");
   const controlTopScroll = control.getBoundingClientRect().top < clientHeight / 2 - 175;
   const container = document.querySelector(".about");
   const containerBottomScroll = container.getBoundingClientRect().bottom < clientHeight - 187;
   const containerTopScroll = container.getBoundingClientRect().bottom < clientHeight;
-  console.log("контейнер низ", containerBottomScroll);
-  console.log("контейнер верх", containerTopScroll);
-  console.log("котрол", controlTopScroll);
+  // console.log("контейнер низ", containerBottomScroll);
+  // console.log("контейнер верх", containerTopScroll);
+  // console.log("котрол", controlTopScroll);
   if (controlTopScroll && !containerTopScroll) {
     wrap.classList.remove("about-building--bottom");
     wrap.classList.add("about-building--fixed");
+    // if (wrap.classList.contains("flag")) {
+    //   wrap.classList.remove("animation");
+    //   wrap.classList.add("animation-reverse");
+    // } else {
+    //   wrap.classList.remove("animation-reverse");
+    //   wrap.classList.add("animation");
+    // }
   } else if (containerTopScroll && controlTopScroll) {
     wrap.classList.add("about-building--bottom");
+    // wrap.classList.add("flag");
+    // wrap.classList.remove("animation");
+    // wrap.classList.remove("animation-reverse");
     wrap.classList.remove("about-building--fixed");
   } else if (!containerBottomScroll && !controlTopScroll && !containerTopScroll) {
     wrap.classList.remove("about-building--bottom");
+    // wrap.classList.remove("flag");
+    // wrap.classList.remove("animation");
+    // wrap.classList.remove("animation-reverse");
     wrap.classList.remove("about-building--fixed");
   }
 }
--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,9 +1,23 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // STYLES=============================================================================
 import '../sass/style.sass';
-import Building from './modules/building';
 
-const building = new Building();
-// import Video from "./modules/video";
-
-// const video = new Video();
+if (document.documentElement.clientWidth > 1025) {
+  let controller = new ScrollMagic.Controller({
+    loglevel: 0,
+  });
+  let scenes = [];
+  let buildAnim = new TimelineMax();
+  buildAnim.to(".js-building .auto", 61, { backgroundPosition: "0px -17690px", ease: SteppedEase.config(61) });
+  scenes.push(new ScrollMagic.Scene({
+    triggerElement: ".js-building-control", duration: '100%', triggerHook: 0.5, offset: 200
+  })
+    .setPin(".js-building")
+    .addTo(controller));
+    scenes.push(new ScrollMagic.Scene({
+      triggerElement: ".js-building-control", duration: '100%', triggerHook: 0.5, offset: 200
+    })
+      .setTween(buildAnim)
+      .addTo(controller));
+}
