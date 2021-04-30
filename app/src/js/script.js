ee17cca72f319846e6c7d2f8292bf0eaa915413b
shoffkarim <k4r1mch1k@gmail.com>
2021-04-30 12:31:49 +0500

    добавил анимацю круговъ

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// STYLES=============================================================================
-import '../sass/style.sass';
-import Cookie from './modules/cookie';
-import InputMask from './modules/imask-init';
-import Popup from './modules/popup';
-import Validation from './modules/validation';
+
+import "../sass/style.sass";
+
+import Cookie from "./modules/cookie";
+import InputMask from "./modules/imask-init";
+import Popup from "./modules/popup";
+import Validation from "./modules/validation";
 
 const popup = new Popup();
 const validation = new Validation();
@@ -18,15 +19,46 @@ if (document.documentElement.clientWidth > 767) {
   });
   let scenes = [];
   let buildAnim = new TimelineMax();
-  buildAnim.to(".js-building .auto", 61, { backgroundPosition: "0px -17690px", ease: SteppedEase.config(61) });
-  scenes.push(new ScrollMagic.Scene({
-    triggerElement: ".js-building-control", duration: '100%', triggerHook: 0.5, offset: 200
-  })
-    .setPin(".js-building")
-    .addTo(controller));
-    scenes.push(new ScrollMagic.Scene({
-      triggerElement: ".js-building-control", duration: '100%', triggerHook: 0.5, offset: 200
+  buildAnim.to(".js-building .auto", 61, {
+    backgroundPosition: "0px -17690px",
+    ease: SteppedEase.config(61),
+  });
+  scenes.push(
+    new ScrollMagic.Scene({
+      triggerElement: ".js-building-control",
+      duration: "100%",
+      triggerHook: 0.5,
+      offset: 200,
+    })
+      .setPin(".js-building")
+      .addTo(controller)
+  );
+  scenes.push(
+    new ScrollMagic.Scene({
+      triggerElement: ".js-building-control",
+      duration: "100%",
+      triggerHook: 0.5,
+      offset: 200,
     })
       .setTween(buildAnim)
-      .addTo(controller));
+      .addTo(controller)
+  );
+  scenes.push(
+    new ScrollMagic.Scene({
+      triggerElement: ".anim-circle--1",
+      triggerHook: 0.5,
+      offset: -300,
+    })
+      .setClassToggle(".anim-circle--1", "scale")
+      .addTo(controller)
+  );
+  scenes.push(
+    new ScrollMagic.Scene({
+      triggerElement: ".anim-circle--2",
+      triggerHook: 0.5,
+      offset: -300,
+    })
+      .setClassToggle(".anim-circle--2", "scale")
+      .addTo(controller)
+  );
 }
