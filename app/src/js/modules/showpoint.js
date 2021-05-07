981dc513b6a0d6f1d72fca78a036fa09caaf9674
shoffkarim <k4r1mch1k@gmail.com>
2021-05-07 12:52:29 +0500

    настройка параметров карты

--- app/src/js/modules/showpoint.js
+++ app/src/js/modules/showpoint.js
@@ -1,19 +1,19 @@
 /* eslint-disable class-methods-use-this */
-class ShowPoint {
-  constructor() {
-    this.init();
-  }
+// class ShowPoint {
+//   constructor() {
+//     this.init();
+//   }
 
-  init() {
-    if (document.querySelector(".js-show-point")) {
-      const btns = document.querySelectorAll(".js-show-point");
-      btns.forEach((btn) => btn.addEventListener("click", function (e) {
-        e.preventDefault();
-        let id = btn.dataset.id;
-        document.querySelector(".ymaps-2-1-78-placemark-overlay").focus();
-      }));
-    }
-  }
-}
+//   init() {
+//     if (document.querySelector(".js-show-point")) {
+//       const btns = document.querySelectorAll(".js-show-point");
+//       btns.forEach((btn) => btn.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         let id = btn.dataset.id;
+//         document.querySelector(".ymaps-2-1-78-placemark-overlay").focus();
+//       }));
+//     }
+//   }
+// }
 
-export default ShowPoint;
+// export default ShowPoint;
