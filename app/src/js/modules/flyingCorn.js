4054529d2a4678521e455e061eb1f48ec4c06485
shoffkarim <k4r1mch1k@gmail.com>
2021-05-11 15:56:37 +0500

    адаптив

--- app/src/js/modules/flyingCorn.js
+++ app/src/js/modules/flyingCorn.js
@@ -7,8 +7,10 @@ class FlyingCorn {
   init() {
     if (document.querySelector(".fly-corn")) {
       const elems = document.querySelectorAll(".fly-elem");
+      const container = document.querySelector(".fly-corn");
+      const height = container.clientHeight;
       elems.forEach(function (item) {
-        item.style.bottom = `${getRandomArbitrary(0, 450)}px`;
+        item.style.bottom = `${getRandomArbitrary(0, height)}px`;
         item.style.animationDuration = `${getRandomArbitrary(2, 5)}s`;
       });
     }
