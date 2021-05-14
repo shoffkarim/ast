51482173f9a70fcce74dc9018f7d58dc3ee834ce
shoffkarim <k4r1mch1k@gmail.com>
2021-05-14 11:00:49 +0500

    lazyload

--- app/src/js/modules/lazyload.js
+++ app/src/js/modules/lazyload.js
@@ -15,21 +15,6 @@ class Lazyload {
         };
       });
     }
-    window.addEventListener("resize", function () {
-      videoloading();
-    });
-    window.addEventListener("load", function () {
-      videoloading();
-    });
-  }
-}
-
-function videoloading() {
-  if (document.querySelector(".lazyload-video") && document.documentElement.clientWidth > 550) {
-    const video = document.querySelector(".lazyload-video");
-    const src = video.dataset.src;
-    video.setAttribute("src", `${src}`);
-    video.parentElement.load();
   }
 }
 
